2494c6ef69c7728ae64e4843fba9403a6d1c64ad
Nagel Raphael <Raphael.NAGEL@htlvb.at>
2022-12-12 09:57:20 +0100

    Read input file

--- AoC2022_Day1/Program.cs
+++ AoC2022_Day1/Program.cs
@@ -1,17 +1,1 @@
-﻿string sample = @"1000
-2000
-3000
-
-4000
-
-5000
-6000
-
-7000
-8000
-9000
-
-10000";
-
-int maxCalories = sample.Split("\n\n").Select(elf => elf.Split("\n").Select(int.Parse).Sum()).Max();
-Console.WriteLine(maxCalories);
+﻿string input = File.ReadAllText("input.txt");
